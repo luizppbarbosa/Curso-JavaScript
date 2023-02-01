f6bca6bb6f76db134f82c269a84243546eaa862e
Luiz Phillip P. Barbosa <luizppbarbosa@gmail.com>
2023-02-01 11:38:36 -0300

    att aula11

---
+++ aula11/ex011.js
@@ -0,0 +1,10 @@
+var vel = 59
+var nome = 'Luiz'
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou a valocidade permitida, você foi MULTADO!`)
+} 
+
+console.log(`${nome} Dirija sempre usando cinto de segurança`)
+
---
+++ aula11/ex012.js
@@ -0,0 +1,7 @@
+var pais = 'a'
+
+if (pais == 'Brasil') {
+    console.log('Brasileiro!')
+} else {
+    console.log('Estrangeiro!')
+}
